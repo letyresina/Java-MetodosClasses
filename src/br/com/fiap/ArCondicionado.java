0f44c44d199242f15de45282d0c8bc6a29d82b7e
letyresina <leticiaresina@gmail.com>
2024-02-23 11:44:52 -0300

    feito classe do ar condicionado

---
+++ src/br/com/fiap/ArCondicionado.java
@@ -0,0 +1,26 @@
+package br.com.fiap;
+
+public class ArCondicionado {
+	// atributo 
+	public int temperatura;
+	public String modo;
+	
+	// metódos
+	public void aumentarTemperatura() {
+		temperatura++;
+	}
+	
+	public void diminuirTemperatura() {
+		temperatura--;
+	}
+	
+	public void trocarModo(String modoUsuario) {
+		// modos: Cool (Frio), Heat (Quente), Auto (Automático), Dry (Desumidificar), Fan (Ventilação)
+		modo = modoUsuario;
+	}
+	
+	public void mostrar() {
+		System.out.println("Temperatura atual: " + temperatura);
+		System.out.println("Modo atual: " + modo);
+	}
+}
