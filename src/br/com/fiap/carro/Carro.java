02191f80444231924205d76f7b9ac8643596f648
letyresina <leticiaresina@gmail.com>
2024-02-26 22:18:40 -0300

    corrigindo diagrama

---
+++ src/br/com/fiap/carro/Carro.java
@@ -0,0 +1,12 @@
+package br.com.fiap.carro;
+
+public class Carro {
+	// atributos
+	public String marca;
+	public String cor;
+	public String tipo;
+	public String modelo;
+	public int anoFabricado;
+	
+	
+}
