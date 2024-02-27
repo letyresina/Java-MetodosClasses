908fac3a16fe6323c2855cf6fa3c7d7650c4bd5d
letyresina <leticiaresina@gmail.com>
2024-02-26 22:34:31 -0300

    finalizado

--- src/br/com/fiap/carro/Carro.java
+++ src/br/com/fiap/carro/Carro.java
@@ -1,6 +1,11 @@
 package br.com.fiap.carro;
 
+import java.util.Scanner;
+
 public class Carro {
+	
+	Scanner scan;
+	
 	// atributos
 	public String marca;
 	public String cor;
@@ -8,5 +13,40 @@ public class Carro {
 	public String modelo;
 	public int anoFabricado;
 	
+	// metódos 
+	public void ligarCarro() {
+		System.out.println("Carro ligado.");
+	}
 	
+	public void desligarCarro() {
+		System.out.println("Carro desligado.");
+	}
+	
+	public void cadastrarCarro() {
+		scan = new Scanner(System.in);
+		
+		System.out.println("Qual a marca do carro?");
+		marca = scan.next();
+		
+		System.out.println("Qual a cor do carro?");
+		cor = scan.next();
+		
+		System.out.println("Qual o tipo de utilização do carro?");
+		tipo = scan.next();
+		
+		System.out.println("Qual o modelo do carro?");
+		modelo = scan.next();
+		
+		System.out.println("Qual ano de fabricação do carro?");
+		anoFabricado = scan.nextInt();
+	}
+	
+	public void mostrar() {
+		System.out.println("Carro cadastrado atualmente");
+		System.out.println("Marca: " + marca);
+		System.out.println("Cor: " + cor);
+		System.out.println("Tipo de utilização: " + tipo);
+		System.out.println("Modelo: " + modelo);
+		System.out.println("Ano de fabricação: " + anoFabricado);
+	}
 }
---
+++ src/br/com/fiap/carro/UsaCarro.java
@@ -0,0 +1,18 @@
+package br.com.fiap.carro;
+
+public class UsaCarro {
+
+	public static void main(String[] args) {
+		
+		Carro carro2 = new Carro();
+		
+		carro2.cadastrarCarro();
+
+		carro2.mostrar();
+		
+		carro2.ligarCarro();
+		
+		carro2.desligarCarro();
+	}
+
+}
